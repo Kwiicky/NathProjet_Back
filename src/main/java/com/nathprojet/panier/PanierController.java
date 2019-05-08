180ea7ad02c9ff5a96aedaf80d41a8893cea5083
Camille aubry <camille.aubry_ext@michelin.com>
2019-05-08 10:30:07 +0200

    back panier

--- src/main/java/com/nathprojet/panier/PanierController.java
+++ src/main/java/com/nathprojet/panier/PanierController.java
@@ -1,10 +1,10 @@
 package com.nathprojet.panier;
 
 import com.nathprojet.panier.dao.PanierBean;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 /**
  * Created by FP13694 on 20/01/2019.
@@ -31,5 +31,19 @@ public class PanierController {
     public void delete(@RequestBody PanierBean panierBean) {
         panierService.delete(panierBean);
     }
+
+    @Transactional
+    @PostMapping()
+    @ResponseBody
+    public PanierBean create(@RequestBody PanierBean article){
+        return panierService.create(article);
+    }
+
+    @Transactional
+    @PutMapping
+    public PanierBean update(@RequestBody PanierBean article) {return panierService.update(article); }
+
+    @GetMapping("/{id}")
+    public Optional<PanierBean> getById(@PathVariable("id") String id) {return  panierService.getById(id);}
 
 }
--- src/main/java/com/nathprojet/panier/dao/PanierRepository.java
+++ src/main/java/com/nathprojet/panier/dao/PanierRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 /**
  * Created by FP13694 on 20/01/2019.
  */
-public interface PanierRepository extends CrudRepository<PanierBean, Integer> {
+public interface PanierRepository extends CrudRepository<PanierBean, String> {
+
 }
